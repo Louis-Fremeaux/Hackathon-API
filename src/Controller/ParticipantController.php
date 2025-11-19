5ddf64c58cd753eab73b52176608fcc43222157f
Louis FREMEAUX <louis.fremeaux@icloud.com>
2025-11-19 12:20:34 +0100

    remove un-used

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class ParticipantController extends AbstractController implements ApiController
+class ParticipantController extends AbstractController //implements ApiController
 {
    /* #[Route('api/participant/{id}', name: 'participant', methods: ['GET'])]
     public function getId(EntityManagerInterface $entityManager, int $id): JsonResponse
